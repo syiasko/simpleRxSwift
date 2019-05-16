99e9e7858ff95970b3e73c5e199a6c7225deeab1
Mendhy Syiasko <syiasko@gmail.com>
2019-05-16 14:11:33 +0700

    Initial Commit

---
+++ SimpleRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleRxSwift
+//
+//  Created by Mendhy Syiasko on 16/05/19.
+//  Copyright © 2019 Mendhy Syiasko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
